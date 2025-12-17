62167118fdf15762ddb0a35a14fa137b86a4170f
shadowrappro <sobirovumidjonobidjonovich@gmail.com>
2025-12-17 14:49:33 +0500

    add fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,10 +13,10 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://unpkg.com/aos@2.3.1/dist/aos.css"
         />
-        {/* <link
+        <link
           href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
           rel="stylesheet"
-        /> */}
+        />
       </head>
       <body>
         {children}
